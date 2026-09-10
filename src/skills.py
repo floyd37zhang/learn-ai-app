aa2dede51f94d33d5e90a336ac75af245e70a3fc
floyd37zhang <2818570771@qq.com>
2026-09-10 15:29:12 +0800

    update skill

--- src/skills.py
+++ src/skills.py
@@ -2,11 +2,13 @@ from langchain_core.tools import tool
 from retriever import get_retriever
 from config import Config
 
+_retriever = get_retriever(Config.CHROMA_DB_PATH)
+
 
 @tool
 def search_knowledge_base(query: str) -> str:
-    retriever = get_retriever(Config.CHROMA_DB_PATH)
-    docs = retriever.invoke(query)
+    """当用户询问关于员工手册的问题时，搜索知识库获取相关信息"""
+    docs = _retriever.invoke(query)
     if not docs:
         return "没有找到相关文档"
     
